c88e0a48d687a8c4ee71435f4f2af9d1ede9c58b
Maxim <farcdoo@gmail.com>
2025-01-09 14:49:32 +0300

    правки

--- src/const.js
+++ src/const.js
@@ -1,11 +1,7 @@
 
-const POINT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
-
-const CITIES = ['Amsterdam', 'Geneva', 'Chamonix', 'Moscow'];
-
 const eventsContainerElement = document.querySelector('.trip-events');
 const tripControlsElement = document.querySelector('.trip-main__trip-controls');
 
-export {POINT_TYPES, CITIES, eventsContainerElement, tripControlsElement};
+export {eventsContainerElement, tripControlsElement};
 
 
